ead8c4bd35902feefcb4cf88fc737060026ecd61
AloneMonkey <liupeiqing1993@163.com>
2018-02-26 15:15:49 +0800

    [update] update the dir

--- gen.sh
+++ gen.sh
@@ -4,10 +4,10 @@ find ./ -name ".DS_Store" -depth -exec rm {} \;
 for file in `ls ./projects`
     do
       if [[ -d "./projects/$file" ]]; then
-        mkdir -p "docs/debs/$file"
-        dpkg-deb -bZgzip "./projects/$file" "docs/debs/$file"
+        mkdir -p "debs/$file"
+        dpkg-deb -bZgzip "./projects/$file" "debs/$file"
 			fi
     done
 
-dpkg-scanpackages -m ./docs/debs > ./docs/Packages
-bzip2 -fks ./docs/Packages
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fks Packages

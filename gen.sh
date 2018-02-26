73ea88912ecaca33823d2ff385e57f2d86e480e5
AloneMonkey <liupeiqing1993@163.com>
2018-02-26 14:34:32 +0800

    [update] add docs dir

--- gen.sh
+++ gen.sh
@@ -4,10 +4,10 @@ find ./ -name ".DS_Store" -depth -exec rm {} \;
 for file in `ls ./projects`
     do
       if [[ -d "./projects/$file" ]]; then
-        mkdir -p "debs/$file"
-        dpkg-deb -bZgzip "./projects/$file" "debs/$file"
+        mkdir -p "docs/debs/$file"
+        dpkg-deb -bZgzip "./projects/$file" "docs/debs/$file"
 			fi
     done
 
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -fks Packages
+dpkg-scanpackages -m ./docs/debs > ./docs/Packages
+bzip2 -fks ./docs/Packages

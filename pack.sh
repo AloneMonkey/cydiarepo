250b4e65b60fdc731b2a11ddc1026e3abd4fc54d
AloneMonkey <liupeiqing1993@163.com>
2018-02-28 21:12:41 +0800

    [update] add pack.sh move package from source to projects

---
+++ pack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for file in `ls ./source`
+    do
+      if [[ -d "./source/$file" ]]; then
+        rm -rf "./projects/$file"
+        mkdir -p "./projects/$file"
+        cp -rf "./source/$file/$file/Package/" "./projects/$file/"
+        echo "successful copy $file to projects folder"
+			fi
+    done
